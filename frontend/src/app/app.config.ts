4e705dcfb458198734d888a2c10fde454f159842
Mike <mxbalogun@gmail.com>
2026-04-24 17:03:52 -0400

    Added Angular Material, defined global theme styles, and updated routes to include sign-up page.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideIcons } from '@ng-icons/core';
+import { heroHeart, heroUser, heroCalendar } from '@ng-icons/heroicons/outline';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideIcons({ heroHeart, heroUser, heroCalendar })
   ]
 };
