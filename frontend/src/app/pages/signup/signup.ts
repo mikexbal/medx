8e500da04479e79f614b164a8db7f447da73f892
Mike <mxbalogun@gmail.com>
2026-04-24 16:59:55 -0400

    Created sign up page

---
+++ frontend/src/app/pages/signup/signup.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NgIconComponent } from '@ng-icons/core';
+
+@Component({
+  selector: 'app-signup',
+  imports: [NgIconComponent],
+  templateUrl: './signup.html',
+  styleUrl: './signup.css',
+})
+export class Signup {}
